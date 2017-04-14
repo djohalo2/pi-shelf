016685a1a5b22f5c7867fe6f93c91b8926653846
Maarten Paauw <maartenpaauw@gmail.com>
2017-04-14 14:23:13 +0200

    Debug

--- Main.py
+++ Main.py
@@ -84,7 +84,7 @@ try:
                 demo = API.demo_gescanned(reader.uuid)
 
         # Controleer of de reader een UUID heeft.
-        if not reader.uuid:
+        if not reader.heeft_uuid():
 
             # Sla de huidige UUID op.
             reader.huidige_uuid = reader.uuid
