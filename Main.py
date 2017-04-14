61b5d973b22fd35c4e24aca3f8cf06f07d2562e4
Maarten Paauw <maartenpaauw@gmail.com>
2017-04-14 14:58:02 +0200

    Debug

--- Main.py
+++ Main.py
@@ -62,12 +62,6 @@ try:
         # Lees de reader uit.
         reader.read()
 
-        # Controleer of de knop losgelaten is en fake ingedrukt is.
-        if not button.is_pressed() and button.is_fake_pressed():
-
-            if reader.read():
-                button.fake_pressed = False
-
         # Controleer of het UUID niet hetzelfde is.
         if reader.uuid != reader.huidige_uuid:
 
@@ -116,9 +110,14 @@ try:
                 # Doe API call.
                 API.knop_ingedrukt(reader.uuid)
 
-            # Zet het fake indrukken op true.
-            button.fake_pressed = True
+                # Zet het fake indrukken op true.
+                button.fake_pressed = True
 
+        # Controleer of de knop losgelaten is en fake ingedrukt is.
+        if not button.is_pressed() and button.is_fake_pressed():
+
+            if reader.read():
+                button.fake_pressed = False
 
         # Wacht 200 milliseconden.
         sleep(0.2)
